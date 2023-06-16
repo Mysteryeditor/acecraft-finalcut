bcf820949ca491f15e146c9d8017e91cae61c43a
Amigosouls <amigosouls022@gmail.com>
2023-06-16 13:01:02 +0530

    dfsdafasf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { TabMenuModule } from 'primeng/tabmenu';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,34 @@
+import { Component ,OnInit} from '@angular/core';
+import { MenuItem } from 'primeng/api';
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit{
+ 
+  items!: MenuItem[];
+
+  activeItem!: MenuItem;
+
+  ngOnInit() {
+      this.items = [
+          { label: 'SCHOOL', styleClass:'navmenu'},
+          { label: 'COLLEGE', icon: 'pi pi-fw pi-calendar' },
+          { label: 'ENTERPRISE', icon: 'pi pi-fw pi-pencil' },
+          { label: 'NOTES', icon: 'pi pi-fw pi-file' },
+          { label: 'AIREN MASK', icon: 'pi pi-fw pi-cog' }
+      ];
+
+      this.activeItem = this.items[0];
+  }
+
+  onActiveItemChange(event:any){
+      this.activeItem = event;
+  }
+
+  activateLast() {
+      this.activeItem = this.items[this.items.length - 1];
+  }
+
+}
