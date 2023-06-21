a2b36983843e95262058cbea34c736a431f6795d
Mysteryeditor <sri027.cvb@gmail.com>
2023-06-21 13:02:48 +0530

    lunch

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
-import { Component,OnInit } from '@angular/core';
-import{FormControl,FormGroup} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormControl, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms'
 @Component({
   selector: 'app-register',
@@ -7,56 +7,56 @@ import { Validators } from '@angular/forms'
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  signup!:FormGroup;
-firstname!:FormControl;
-lastname!:FormControl;
-email!:FormControl;
-password!:FormControl;
-confirmpassword!:FormControl
-// school or enterprise radiobutton
-dealername!:FormControl;
-dealernumber!:FormControl;
-city!:FormControl;
-state!:FormControl;
-statelist=[
-  "Andaman and Nicobar Islands",
-  "Andhra Pradesh",
-  "Arunachal Pradesh",
-  "Assam",
-  "Bihar",
-  "Chandigarh",
-  "Chhattisgarh",
-  "Dadra and Nagar Haveli",
-  "Daman and Diu",
-  "Delhi",
-  "Goa",
-  "Gujarat",
-  "Haryana",
-  "Himachal Pradesh",
-  "Jammu and Kashmir",
-  "Jharkhand",
-  "Karnataka",
-  "Kerala",
-  "Ladakh",
-  "Lakshadweep",
-  "Madhya Pradesh",
-  "Maharashtra",
-  "Manipur",
-  "Meghalaya",
-  "Mizoram",
-  "Nagaland",
-  "Odisha",
-  "Puducherry",
-  "Punjab",
-  "Rajasthan",
-  "Sikkim",
-  "Tamil Nadu",
-  "Telangana",
-  "Tripura",
-  "Uttar Pradesh",
-  "Uttarakhand",
-  "West Bengal"
-];
+  signup!: FormGroup;
+  firstname!: FormControl;
+  lastname!: FormControl;
+  email!: FormControl;
+  password!: FormControl;
+  confirmpassword!: FormControl
+  schenter!:FormControl
+  dealername!: FormControl;
+  dealernumber!: FormControl;
+  city!: FormControl;
+  statename!: FormControl;
+  statelist = [
+    "Andaman and Nicobar Islands",
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chandigarh",
+    "Chhattisgarh",
+    "Dadra and Nagar Haveli",
+    "Daman and Diu",
+    "Delhi",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jammu and Kashmir",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Ladakh",
+    "Lakshadweep",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Puducherry",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal"
+  ];
 
 
 
@@ -65,43 +65,52 @@ statelist=[
 
 
 
-gstnumber!:FormControl;
+  gstnumber!: FormControl;
 
 
 
- ngOnInit(): void {
-     this.firstname=new FormControl('',Validators.required);
-     this.lastname=new FormControl('',Validators.required);
-     this.email=new FormControl('',[Validators.required,
-    Validators.email])
-    this.password=new FormControl('',[Validators.required,Validators.minLength(8)]),
-    this.confirmpassword=new FormControl('',[Validators.required])
-    // radiobutton
-    this.dealername=new FormControl('',Validators.required);
-    this.dealernumber=new FormControl('',Validators.required)
-    this.city=new FormControl('',Validators.required),
-    //state
-   
+  ngOnInit(): void {
+    this.firstname = new FormControl('', [Validators.required,
+    Validators.pattern('[a-zA-Z ]*'),
+    Validators.maxLength(20),
+    Validators.minLength(3)
+    ]);
+    this.lastname = new FormControl('', [Validators.required,
+    Validators.pattern('[a-zA-Z ]*'),
+    Validators.maxLength(20),
+    Validators.minLength(3)
+    ]);
+    this.email = new FormControl('', [Validators.required, Validators.email])//pattern
+    this.password=new FormControl('',[Validators.required,Validators.minLength(8)]),//pattern
+    // this.confirmpassword=new FormControl('',[Validators.required])
+    this.schenter=new FormControl('',[Validators.required])
+    this.dealername=new FormControl('',[Validators.required,Validators.minLength(3)]);
+    this.dealernumber=new FormControl('',[Validators.required])
+    this.city=new FormControl('',[Validators.required]),
+    this.statename=new FormControl('',[Validators.required]);
+
     this.gstnumber=new FormControl('',Validators.required)
 
 
-     this.signup=new FormGroup({
-      firstname:this.firstname,
-      lastname:this.lastname,
-      email:this.email,
+    this.signup = new FormGroup({
+      firstname: this.firstname,
+      lastname: this.lastname,
+      email: this.email,
       password:this.password,
-      confirmpassword:this.confirmpassword,
-      dealername:this.dealername,
+      // confirmpassword:this.confirmpassword,
+      schenter:this.schenter,
+       dealername:this.dealername,
       dealernumber:this.dealernumber,
       city:this.city,
+      statename:this.statename,
       gstnumber:this.gstnumber
 
-     })
- }
+    })
+  }
 
 
- onSubmit(){
+  onSubmit() {
 
-  console.log("helo");
-}
+    console.log("helo");
+  }
 }
